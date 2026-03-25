435219bff23e506f4c04718dcd2883608e987379
elrakita <olenarakita@example.com>
2026-03-25 11:39:38 +0200

    Redo login page so that it uses material card

--- client/FinanceTrackerClient/src/app/auth/login/login.ts
+++ client/FinanceTrackerClient/src/app/auth/login/login.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, 
+    RouterLink, 
+    MatCardModule, 
+    MatIconModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
